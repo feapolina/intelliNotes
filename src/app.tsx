527ff4dfb5bd04fcc16e8b25581f3c230bccaba9
Felipe Apolinário <112521745+feapolina@users.noreply.github.com>
2024-02-06 17:32:05 -0300

    ✨ Adiciona componentes de diálogo e funcionalidades de notas

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,12 @@ import imgLogo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
+
+const note = {
+  date: new Date(),
+  content: "Hello World!",
+};
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -21,10 +27,7 @@ export function App() {
       {/* Card Container */}
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px] ">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
       </div>
     </div>
   );
